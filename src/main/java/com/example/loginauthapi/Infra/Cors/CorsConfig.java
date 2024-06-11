8815a13412a006e133db4d63340fe13fa6d03c83
Marcos Vinicius <mdominique2011@gmail.com>
2024-06-11 00:40:41 -0300

    Update CorsConfig.java

--- src/main/java/com/example/loginauthapi/Infra/Cors/CorsConfig.java
+++ src/main/java/com/example/loginauthapi/Infra/Cors/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://angular-spring-seven.vercel.app/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
